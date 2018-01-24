4f5ba4d4370129a8696fbe547be92545485a87eb
N-Kelkay <natikelkay@gmail.com>
2018-01-24 18:57:40 -0500

    Created scene

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		JFrame frame = new JFrame("HelloWorldSwing");
+		frame.setSize(400,400);
+		final JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
